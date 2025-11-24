ff3994abee28a29a6fdaf0c4424f97d6ea2bce99
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2025-11-24 23:44:04 +0100

    Refactor: update form structure and styles for improved user experience

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
-import "./style.css";
-const q = (s)=> document.querySelector(s);
-const qa = (s)=> document.querySelectorAll(s);
-const id = (id)=> document.getElementById(id);
-const className = (cls)=> document.getElementsByClassName(cls);
-// FOR THE ENGAGING MOVES IN THE UI
+const q = (s) => document.querySelector(s);
+const qa = (s) => document.querySelectorAll(s);
+const id = (id) => document.getElementById(id);
+const className = (cls) => document.getElementsByClassName(cls);
+
 const moveSections = qa(".transform");
 const scales = qa(".scale");
-console.log(moveSections);
+const logBtns = qa(".signIn");
+const signInForm = id("formSignIn");
+const signUpForm = id("formSignUp");
+const signUpBtns = qa(".sign-up-btn");
 
+logBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    signInForm.classList.add("show");
+    signUpForm.classList.remove("show");
+  });
+});
+
+
+signUpBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    signUpForm.classList.add("show");
+    signInForm.classList.remove("show");
+  });
+});
+
+// FOR THE ENGAGING MOVES IN THE UI
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-        entry.target.classList.toggle("control");
+      entry.target.classList.toggle("control");
     }
-});
+  });
 });
 moveSections.forEach((move) => {
-    setTimeout(() => {
+  setTimeout(() => {
     observer.observe(move);
-}, 5000);
+  }, 3000);
 });
 scales.forEach((move) => {
-    setTimeout(() => {
+  setTimeout(() => {
     observer.observe(move);
-}, 5000);
+  }, 5000);
 });
